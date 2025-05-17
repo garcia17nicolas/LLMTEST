9e06a2b69ea50e693603964edcbc32c8f2efa578
garcia17nicolas <nicolasgarcia@unicomfacauca.edu.co>
2025-05-17 14:11:23 -0500

    tercera prueba

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ app = FastAPI()
 
 app.include_router(aiRouter.router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+
 @app.get("/")
 def index():
     return {"message": "API is running"}
