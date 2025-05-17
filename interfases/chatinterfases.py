44cc7d23c960dfe07dd803919ec334d59131d336
garcia17nicolas <nicolasgarcia@unicomfacauca.edu.co>
2025-05-17 14:06:10 -0500

    segunda prueba

--- interfases/chatinterfases.py
+++ interfases/chatinterfases.py
@@ -2,4 +2,15 @@ from pydantic import BaseModel
 
 class InputMessage(BaseModel):
     message: str
-   
+class Usage(BaseModel):
+    prompt_tokens: int
+    completion_tokens: int
+    total_tokens: int
+class Message(BaseModel):
+    content: str
+class Choices(BaseModel):
+    message: Message
+class ChatCompletionResponse(BaseModel):
+    model: str
+    choices: list[Choices]
+    usage: Usage
